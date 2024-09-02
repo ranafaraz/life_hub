c149cc65082898a11e87b0ee4c7fa871bd462445
hamza.amjad <hamza.amjad@iub.edu.pk>
2024-09-02 15:17:56 +0500

    Excel file upload work done

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,7 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'welcome';
-
+$route['file_upload'] = 'welcome/file_upload';
 // Auto-routing of controller/method/params
 $route['(:any)'] = '$1';
 $route['(:any)/(:any)'] = '$1/$2';
